bc757b0482d7febda1dbdac34c5f3cc3591e01a0
xfeusw <khamrakulovkamron@gmail.com>
2026-09-19 01:37:03 +0500

    feat: added formatter and reduced boilerplate

--- flake.nix
+++ flake.nix
@@ -4,40 +4,63 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
       systems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
 
-      perSystem = { pkgs, ... }:
+      perSystem =
+        { pkgs, ... }:
         let
-          mkTheme = resolution: pkgs.stdenvNoCC.mkDerivation {
-            pname = "space-isolation-${resolution}";
-            version = "unstable";
+          resolutions = [
+            "1920x1080"
+            "1920x1200"
+            "2560x1440"
+          ];
 
-            src = ./.;
+          mkTheme =
+            resolution:
+            pkgs.stdenvNoCC.mkDerivation {
+              pname = "space-isolation-${resolution}";
+              version = "unstable";
 
-            installPhase = ''
-              mkdir -p $out/share/grub/themes/space-isolation
-              cp -r ${resolution}/* $out/share/grub/themes/space-isolation/
-            '';
+              src = ./.;
 
-            meta = {
-              description = "Space Isolation GRUB theme for ${resolution}";
-              homepage = "https://github.com/xfeusw/space-isolation";
-              license = pkgs.lib.licenses.mit;
-              platforms = pkgs.lib.platforms.linux;
+              installPhase = ''
+                mkdir -p $out/share/grub/themes/space-isolation
+                cp -r ${resolution}/* $out/share/grub/themes/space-isolation/
+              '';
+
+              meta = {
+                description = "Space Isolation GRUB theme for ${resolution}";
+                homepage = "https://github.com/xfeusw/space-isolation";
+                license = pkgs.lib.licenses.mit;
+                platforms = pkgs.lib.platforms.linux;
+              };
             };
-          };
-        in {
-          packages = {
-            theme-1920x1080 = mkTheme "1920x1080";
-            theme-1920x1200 = mkTheme "1920x1200";
-            theme-2560x1440 = mkTheme "2560x1440";
+
+          themes = builtins.listToAttrs (
+            map (resolution: {
+              name = "theme-${resolution}";
+              value = mkTheme resolution;
+            }) resolutions
+          );
+        in
+        {
+          packages = themes;
+
+          treefmt = {
+            programs.nixfmt.enable = true;
           };
         };
     };
